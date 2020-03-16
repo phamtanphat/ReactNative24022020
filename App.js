2338c515be0261eaddf06b0fa13c01f87d7e56e2
phamtanphat <phatdroid94@gmail.com>
2020-03-16 04:12:56 -0700

    flex part 1 : chia deu va chia khong deu kich thuoc

--- App.js
+++ App.js
@@ -1,114 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle1}>View 1</Text>
+        <Text style={styles.textStyle2}>View 2</Text>
+        <Text style={styles.textStyle3}>View 3</Text>
+        <Text style={styles.textStyle4}>View 4</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flexDirection: 'column',
+    flex: 10,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  textStyle1: {
+    flex: 1,
+    color: '#f20',
+    fontSize: 20,
+    fontStyle: 'italic',
+    backgroundColor: 'deepskyblue',
   },
-  body: {
-    backgroundColor: Colors.white,
+  textStyle2: {
+    flex: 3,
+    color: 'darkorange',
+    fontSize: 20,
+    fontStyle: 'italic',
+    backgroundColor: 'darkslateblue',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  textStyle3: {
+    flex: 6,
+    color: 'darkorange',
+    fontSize: 20,
+    fontStyle: 'italic',
+    backgroundColor: 'green',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  textStyle4: {
+    height: 200,
+    color: 'darkorange',
+    fontSize: 20,
+    fontStyle: 'italic',
+    backgroundColor: 'pink',
   },
 });
-
-export default App;
