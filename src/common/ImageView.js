f92b07fe8411038a72adf5258fe347d0e23de9c9
phamtanphat <phatdroid94@gmail.com>
2020-03-20 04:03:06 -0700

    image

--- src/common/ImageView.js
+++ src/common/ImageView.js
@@ -5,11 +5,9 @@ export default class ImageView extends Component {
   render() {
     return (
       <Image
-        style={{width: 100, height: 100}}
-        source={{
-          uri:
-            'https://techtalk.vn/wp-content/uploads/2018/12/react-native.png',
-        }}
+        style={{width : 300 , height : 300}}
+        resizeMode="contain"
+        source={require('../assets/hinh1.jpeg')}
       />
     );
   }
