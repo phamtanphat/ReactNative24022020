22cb5ed49678cdf032c052485e20e11ff1a18399
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-08 19:33:33 +0700

    gan remote devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
 import {createStore} from 'redux';
 import reducer from './reducer/rootReducer';
+import {composeWithDevTools} from 'remote-redux-devtools';
 
-const store = createStore(reducer);
+const composeEnhancers = composeWithDevTools({
+  realtime: true,
+  port: 8000,
+  hostname: 'localhost', //add your computer's IP
+});
+const store = createStore(reducer, composeEnhancers());
 
 export default store;
