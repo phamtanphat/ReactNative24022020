cd169d3244e61030d55c043838ecfd358911d27c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-20 19:20:15 +0700

    chuc nang navigation

--- src/screen/ScreenA.js
+++ src/screen/ScreenA.js
@@ -2,15 +2,30 @@ import React, {Component} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class ScreenA extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Màn hinh A',
+    };
+  }
+  updateTitle = text => {
+    this.setState({text});
+  };
   render() {
     return (
       <View
         style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
-        <Text> Màn hình A </Text>
-        <TouchableOpacity style={{backgroundColor: 'blue'}}>
+        <Text> {this.state.text} </Text>
+        <TouchableOpacity
+          onPress={() =>
+            this.props.navigation.navigate('ScreenB', {
+              updateTitle: this.updateTitle,
+            })
+          }
+          style={{backgroundColor: 'blue'}}>
           <Text>Click</Text>
         </TouchableOpacity>
       </View>
     );
   }
-};
+}
--- src/screen/ScreenB.js
+++ src/screen/ScreenB.js
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class ScreenB extends Component {
+  componentWillUnmount = () => {
+    console.log('componentWillUnmount B');
+  };
   render() {
     return (
       <View
         style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
         <Text> Màn hình B </Text>
-        <TouchableOpacity style={{backgroundColor: 'red'}}>
+        <Text>
+          Giá trị nhận được {this.props.route.params?.text ?? 'value'}
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.route?.params.updateTitle('Cập nhật màn hình A');
+            this.props.navigation.pop();
+          }}
+          style={{backgroundColor: 'red'}}>
           <Text>Click</Text>
         </TouchableOpacity>
       </View>
