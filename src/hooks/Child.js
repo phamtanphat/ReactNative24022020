4e382a5e444fd5c39291148de7943617c93afe81
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-22 18:40:06 +0700

    state

--- src/hooks/Child.js
+++ src/hooks/Child.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const Child = () => {
+const Child = props => {
+  const {en, vn} = props;
   return (
-    <View style={{flexDirection: 'row', justifyContent : 'space-around'}}>
-      <Text>One</Text>
-      <Text>Mot</Text>
+    <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+      <Text>{en}</Text>
+      <Text>{vn}</Text>
     </View>
   );
 };
--- src/hooks/Parent.js
+++ src/hooks/Parent.js
@@ -1,12 +1,31 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, Text} from 'react-native';
 import Child from './Child';
 
 const Parent = () => {
+  const [arrayColor, setarrayColor] = useState([
+    'red',
+    'green',
+    'blue',
+    'orange',
+    'black',
+  ]);
+  const [color, setColor] = useState('');
+
+  function randomColor() {
+    const index = Math.floor(Math.random() * 5);
+    setColor(arrayColor[index]);
+  }
+  console.log('Parent Render');
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Child />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity
+        onPress={randomColor}
+        style={{backgroundColor: 'gray', padding: 10, marginBottom: 10}}>
+        <Text style={{color: 'white', fontSize: 20}}>Random color</Text>
+      </TouchableOpacity>
+      <View style={{backgroundColor: color, width: 300, height: 300}} />
     </View>
   );
 };
